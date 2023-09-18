5bfe2ceef5e3bd185707f142e0d3ef84885dc4b8
Vadim Savinov <vasaykh@yandex.ru>
2023-09-18 19:33:04 +0500

    fix: ecosistem.config.jsbackend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,8 +20,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
